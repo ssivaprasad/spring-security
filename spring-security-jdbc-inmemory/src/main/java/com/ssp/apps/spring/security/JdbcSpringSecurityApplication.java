5cd3907fc891ce1bdb557f752e3f56fd0f2ab5af
sivaprasads <somarouthu.sivaprasads@gmail.com>
2022-12-20 05:32:08 +0530

    spring-security-jdbc-inmemory

---
+++ spring-security-jdbc-inmemory/src/main/java/com/ssp/apps/spring/security/JdbcSpringSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.ssp.apps.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcSpringSecurityApplication.class, args);
+    }
+}
---
+++ spring-security-jdbc-inmemory/src/main/java/com/ssp/apps/spring/security/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.ssp.apps.spring.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //@formatter:off
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("siva").password("siva").roles(new String[]{"USER"}))
+                .withUser(User.withUsername("prasad").password("prasad").roles(new String[]{"USER", "ADMIN"}));
+        //@formatter:on
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //@formatter:off
+        http.authorizeRequests()
+                .antMatchers("/heap-dump").hasRole("ADMIN")
+                .antMatchers("/system-metricks").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+        //@formatter:on
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //@formatter:off
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+        //@formatter:on
+    }
+}
